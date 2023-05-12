1133e7ef71093383b73595fa003742d5d913511f
aluno <Aluno@PR02-402-17.CSED.LAB>
2023-05-12 11:36:09 -0300

    react

---
+++ projetotypescript1/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Soma from './components/soma';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Projeto em React</h1>
+      <Soma></Soma>
+    </div>
+    
+  );
+}
+
+export default App;
+
